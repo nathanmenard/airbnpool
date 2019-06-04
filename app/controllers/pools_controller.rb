5ba8979d10a79347c0633961704b849f7cee9530
nicolas goarant <nicolas.goarant@hotmail.fr>
2019-06-04 18:28:33 +0200

    show

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -7,26 +7,9 @@ class PoolsController < ApplicationController
     @pool = Pool.find(params[:id])
   end
 
-  # def new
-  #   @cocktail = Cocktail.new
-  # end
-
-  # def create
-  #   @cocktail = Cocktail.new(cocktail_params)
-
-  #   @cocktail.save
-
-  #   if @cocktail.save
-  #     # 3. Redirige vers la fiche de cocktail cree
-  #     redirect_to cocktail_path(@cocktail)
-  #   else
-  #     render :new
-  #   end
-  # end
-
   private
 
   def pool_params
-    params.require(:pool).permit(:title, :description, :capacity, :picture, :address, :latitude, :longitude, :heated, :daily_price, :user_id)
+    params.require(:pool).permit(:title, :description, :picture, :address, :latitude, :longitude, :heated, :daily_price, :user_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
-
-
-
-
-
-# mint = Ingredient.create!(name: 'Mint')
-# lime = Ingredient.create!(name: 'Lime')
-# rum = Ingredient.create!(name: 'Rum')
-# sugar = Ingredient.create!(name: 'Sugar')
-
-
-# mojito = Cocktail.create!(name: 'Mojito')
-
-# Dose.create!(cocktail: mojito, ingredient: mint, description: '2 leaves')
-# Dose.create!(cocktail: mojito, ingredient: lime, description: '2 slices')
-# Dose.create!(cocktail: mojito, ingredient: rum, description: '20 cl')
-
-# ti_punch = Cocktail.create!(name: 'Ti Punch')
-
-# Dose.create!(cocktail: ti_punch, ingredient: sugar, description: '2 spoons')
-# Dose.create!(cocktail: ti_punch, ingredient: lime, description: '2 slices')
-# Dose.create!(cocktail: ti_punch, ingredient: rum, description: '20 cl')
-
-
 Booking.destroy_all
 Pool.destroy_all
 User.destroy_all
@@ -41,7 +8,7 @@ nicolas = User.create!(email: 'Nicolas.Goarant@hotmail.fr', password: "secret", 
 nathan = User.create!(email: 'NathanMenard.work@gmail.com', password: "secret", first_name: 'Nathan', last_name: 'Menard')
 maella = User.create!(email: 'mdegras@myrezapp.com', password: "secret", first_name: 'Maella', last_name: 'Degras')
 
-nathan_pool = Pool.create!(user: nathan, title: 'A pool closed for all your family', description: 'nice pool for all the family', heated: false, picture: File.open(Rails.root.join('db/fixtures/pools/piscine1.jpg')), daily_price: 15)
+nathan_pool = Pool.create!(user: nathan, title: 'A pool for all your family', description: 'nice pool for all the family', heated: false, picture: File.open(Rails.root.join('db/fixtures/pools/piscine1.jpg')), daily_price: 15)
 maella_pool = Pool.create!(user: maella, title: 'A great pool closed to the forest', description: 'a sunny pool with a great barbecue', heated: true, picture: File.open(Rails.root.join('db/fixtures/pools/pool.jpg')), daily_price: 20)
 
 
